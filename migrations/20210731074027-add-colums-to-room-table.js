a8b18482235a57eb9d80bfe75423f8ad3fae012a
datnguyen199 <nnd.nguyennhudat@gmail.com>
2021-11-15 11:34:30 +0700

    test search room api

--- migrations/20210731074027-add-colums-to-room-table.js
+++ migrations/20210731074027-add-colums-to-room-table.js
@@ -4,21 +4,21 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(t => {
       return Promise.all([
-        queryInterface.addColumn('Rooms', 'status', {
-          type: Sequelize.DataTypes.INTEGER,
-          default: 0
-        }, { transaction: t }),
-        queryInterface.addColumn('Rooms', 'capacity', {
-          type: Sequelize.DataTypes.INTEGER,
-        }, { transaction: t })
+        // queryInterface.addColumn('Rooms', 'status', {
+        //   type: Sequelize.DataTypes.INTEGER,
+        //   default: 0
+        // }, { transaction: t }),
+        // queryInterface.addColumn('Rooms', 'capacity', {
+        //   type: Sequelize.DataTypes.INTEGER,
+        // }, { transaction: t })
       ]);
     });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.sequelize.transaction(t => {
       return Promise.all([
-        queryInterface.removeColumn('Rooms', 'status', { transaction: t }),
-        queryInterface.removeColumn('Rooms', 'capacity', { transaction: t })
+        // queryInterface.removeColumn('Rooms', 'status', { transaction: t }),
+        // queryInterface.removeColumn('Rooms', 'capacity', { transaction: t })
       ]);
     });
   }
